2edc1eb016906d3ac620d5938357cb2c1fe67cbf
somikaaaA <s0mik.a@yandex.ru>
2025-03-24 21:10:34 +0300

    make methods for Vote

--- src/main/java/com/samarina/model/Vote.java
+++ src/main/java/com/samarina/model/Vote.java
@@ -12,10 +12,37 @@ public class Vote {
     private String name; // название
     private String description; // описание
     private Map<String, Integer> options; //варианты ответов и кол-во голосов
+    private String creator; // создатель голосования
 
     public Vote(String name, String description) {
         this.name = name;
         this.description = description;
         this.options = new HashMap<>();
+    }
+
+    public void addOption(String option) {
+        options.put(option, 0); // изначально количество голосов для варианта равно 0
+    }
+
+    public void voteForOption(String option) {
+        if (options.containsKey(option)) {
+            options.put(option, options.get(option) + 1);
+        }
+    }
+
+    public boolean removeOption(String option) {
+        return options.remove(option) != null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Голосование: ").append(name).append("\n");
+        sb.append("Описание: ").append(description).append("\n");
+        sb.append("Варианты ответа:\n");
+        for (Map.Entry<String, Integer> entry : options.entrySet()) {
+            sb.append(" - ").append(entry.getKey()).append(": ").append(entry.getValue()).append(" votes\n");
+        }
+        return sb.toString();
     }
 }
