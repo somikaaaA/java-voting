3262b7527285222d28ec8494eb8bb4d9d7e77225
somikaaaA <s0mik.a@yandex.ru>
2025-03-24 21:04:44 +0300

    update model, add lombok

--- src/main/java/com/samarina/model/Topic.java
+++ src/main/java/com/samarina/model/Topic.java
@@ -1,8 +1,19 @@
 package com.samarina.model;
 
+import lombok.Getter;
+
+import java.util.HashMap;
 import java.util.Map;
 
 public class Topic {
+    @Getter
     private String name; // название
     private Map<String, Vote> votes; // голосования по разделу (ключ-голосование)
+
+    public Topic(String name) {
+        this.name = name;
+        this.votes = new HashMap<>();
+    }
+
+
 }
--- src/main/java/com/samarina/model/Vote.java
+++ src/main/java/com/samarina/model/Vote.java
@@ -1,9 +1,21 @@
 package com.samarina.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
 import java.util.Map;
 
+@Getter
+@Setter
 public class Vote {
     private String name; // название
     private String description; // описание
-    private Map<String, Integer> options; //варианты ответов
+    private Map<String, Integer> options; //варианты ответов и кол-во голосов
+
+    public Vote(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.options = new HashMap<>();
+    }
 }
